4b2381f67765b9e8b05508c51bab2fc85ebd58fa
Dongri Jin <dongrify@gmail.com>
2022-08-29 15:25:38 +0900

    Add EIP 55

--- ethereum-wallet-generator/index.js
+++ ethereum-wallet-generator/index.js
@@ -15,7 +15,7 @@ console.log();
 for (let i = 0; i < 10; i++) {
   let wallet = hdwallet.derivePath(wallet_hdpath + i).getWallet();
   let privateKey = wallet.getPrivateKeyString();
-  let address = wallet.getAddressString();
+  let address = wallet.getChecksumAddressString(); // EIP-55
   console.log('private key ' + i + ': ' + privateKey);
   console.log('address     ' + i + ': ' + address);
   console.log();
